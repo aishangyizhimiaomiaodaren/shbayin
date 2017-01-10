bf6ed0c00fa4470afab94f2262063c717f25fcc0
miaomiaodaren <979855682@qq.com>
2017-01-10 11:33:41 +0800

    sh

---
+++ src/shbayin/Test.java
@@ -0,0 +1,17 @@
+/**
+ * 
+ */
+package shbayin;
+
+/**
+ *类名称:
+ *类描述:
+ *创建人:xutao
+ *创建时间:2017年1月10日 上午11:31:48
+ * 
+ */
+public class Test {
+	public void main(String args[]){
+		System.out.println("Hello World");
+	}
+}
